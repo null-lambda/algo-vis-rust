e243e701f816213d6577f200bcbe6b71ecf140b6
null_lambda <your_donotreply_email_id>
2024-10-19 17:27:30 +0900

    add console_log

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use wasm_bindgen::prelude::*;
+
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
@@ -8,3 +10,18 @@ pub fn set_panic_hook() {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 }
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
+}
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (crate::utils::log(&format_args!($($t)*).to_string()))
+}
+
+pub(crate) use console_log;
+use web_sys::js_sys::wasm_bindgen;

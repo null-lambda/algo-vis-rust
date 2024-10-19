acb46f657793c7bc4c3ebc8ae1f038de3994ee47
null_lambda <your_donotreply_email_id>
2024-10-19 13:01:30 +0900

    add voronoi.rs
    add base logic code, unfinished

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 mod utils;
+mod voronoi;
 
+use rand::{rngs::SmallRng, Rng, SeedableRng};
+use voronoi::geometry::Point;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -9,5 +12,19 @@ extern "C" {
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, algo-vis-rust!");
+    let mut rng = SmallRng::from_entropy();
+    let mut builder = voronoi::builder::Builder::new();
+    let n = 20;
+    let mut points = vec![];
+    for _ in 0..n {
+        let x: i32 = rng.gen_range(0..100);
+        let y: i32 = rng.gen_range(0..100);
+        points.push(Point::new(x as f64, y as f64));
+    }
+
+    builder.add_points(points);
+    let s = format!("{:?}", builder.graph.topo);
+
+    builder.run();
+    alert(&s);
 }

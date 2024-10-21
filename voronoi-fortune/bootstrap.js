3f4e0a1b5590685f9639309584c76ba5cc93b79a
null_lambda <your_donotreply_email_id>
2024-10-21 16:35:33 +0900

    canvas

--- voronoi-fortune/bootstrap.js
+++ voronoi-fortune/bootstrap.js
@@ -249,7 +249,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/algo_vis_rust_bg.wasm":"c3ea94f85b17843d225b"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/algo_vis_rust_bg.wasm":"d6e018da95bcf4182745"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
